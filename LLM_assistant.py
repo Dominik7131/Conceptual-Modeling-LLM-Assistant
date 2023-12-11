a9916f5bd53de51366847b8bfc29adc00c5e06df
Dominik7131 <dom.mod@seznam.cz>
2023-12-11 19:16:16 +0100

    File updated

--- LLM_assistant.py
+++ LLM_assistant.py
@@ -10,7 +10,7 @@ ITEMS_COUNT = 5
 IS_SYSTEM_MSG = True
 IS_CONCEPTUAL_MODEL_DEFINITION = False
 IS_IGNORE_DOMAIN_DESCRIPTION = False
-TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION = False
+TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION = True
 
 ATTRIBUTES_STRING = "attributes"
 RELATIONSHIPS_STRING = "relationships"
@@ -41,17 +41,24 @@ class LLMAssistant:
                 #system = "You are creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name which can be either a single verb or a verb and preposition such that when you insert this name in between the source entity and the target entity in this order a short meaningful sentence is created."
                 #system = "You are creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name which is represented as single verb in singular such that when you insert this relationship name in between the source entity and the target entity in this order a short meaningful sentence is created."
                 #if is_source_entity:
-                system = "You are creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name such that when you insert it in between the source entity and the target entity in this order a short meaningful sentence is created. When you come up with a new relationship name always make sure that the described short meaningful sentence can be created."
+                system = "You are an expert at creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name such that when you insert it in between the source entity and the target entity in this order a short meaningful sentence is created. When you come up with a new relationship name always make sure that the described short meaningful sentence can be created."
                 #else:
                     #system = "You are creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name such that when you insert it in between the source entity and the target entity in this order a short meaningful sentence is created. You will be given a target entity and your goal is to come up with a source entity and a new relationship name. Always make sure that the described short meaningful sentence can be created."
             else:
-                system = "You are an expert at extracting relationships for a given entity solely based on a given text in context of creating conceptual model in software engineering."
+                #system = "You are an expert at extracting relationships for a given entity solely based on a given text in context of creating conceptual model in software engineering."
+                #system = "You are creating a conceptual model which consists of entities and their relationships. "
+                #system += "Each relationship is between exactly two entities, we will describe them as the source entity and the target entity. "
+                #system += "Each relationship has a name in a verb form such that when you insert this verb in between the source entity and the target entity in this order a short meaningful sentence is created. "
+                #system += "Always make sure that the short meaningful sentence indeed makes sense. Be very careful when creating the short meaningful sentence: the source entity must come first then follows the relationship name and then follows the target entity name which ends the sentence. Always check that this order holds."
 
+                system = "You are an expert creating a conceptual model which consists of entities and their relationships. Each relationship is between exactly two entities, we will denote them as the source entity and the target entity. Both entities are represented as nouns in singular. Each relationship has a name such that when you insert it in between the source entity and the target entity in this order a short meaningful sentence is created. When you come up with a new relationship name and a new target entity always make sure that the described short meaningful sentence can be created."
+
+                #system += " You will be given a source entity and your goal is to solely based on the given text to find a relationships between this source entity and some new target entity."
 
 
 
         elif user_choice == RELATIONSHIPS_STRING_TWO_ENTITIES:
-            system = "You are creating a conceptual model which consists of entities and their relationships. "
+            system = "You are an expert at creating a conceptual model which consists of entities and their relationships. "
             system += "Each relationship is between exactly two entities, we will describe them as the source entity and the target entity. "
             system += "Each relationship has a name in a verb form such that when you insert this verb in between the source entity and the target entity in this order a short meaningful sentence is created. "
             system += "Always make sure that the short meaningful sentence indeed makes sense. Be very careful when creating the short meaningful sentence: the source entity must come first then follows the relationship name and then follows the target entity name which ends the sentence. Always check that this order holds."
@@ -94,10 +101,14 @@ class LLMAssistant:
                 print(f"- Description: {completed_item['description']}")
 
         elif user_choice == RELATIONSHIPS_STRING:
-            if is_provided_class_source and completed_item['source'] != user_input_entity_name:
-                print(f"Warning: source entity is: {completed_item['source']}")
-            elif not is_provided_class_source and completed_item['target'] != user_input_entity_name:
-                print(f"Warning: target entity is: {completed_item['target']}")
+            #if is_provided_class_source and completed_item['source'] != user_input_entity_name:
+            #    print(f"Warning: source entity is: {completed_item['source']}")
+            #elif not is_provided_class_source and completed_item['target'] != user_input_entity_name:
+            #    print(f"Warning: target entity is: {completed_item['target']}")
+
+            if user_input_entity_name != completed_item['source'] and user_input_entity_name != completed_item['target']:
+                self.end_parsing_prematurely = True
+                return completed_item
 
             print(f"{len(items) + 1}: {completed_item['name'].capitalize()}")
 
@@ -129,6 +140,7 @@ class LLMAssistant:
         is_skip_parsing = False
         new_lines_in_a_row = 0
         last_char = ''
+        self.end_parsing_prematurely = False
 
         for text in self.llm(prompt, stream=True):
             assistant_message += text
@@ -161,6 +173,11 @@ class LLMAssistant:
                 if char == "}" and item != '':
                     is_item_start = False
                     completed_item = self.parse_item(item, items, user_choice, is_provided_class_source, user_input_entity_name)
+
+                    if self.end_parsing_prematurely:
+                        print(f"Ending parsing prematurely: {completed_item}")
+                        return items
+                        
                     items.append(completed_item)
                     item = ""
                 
@@ -227,30 +244,35 @@ class LLMAssistant:
         
         elif user_choice == RELATIONSHIPS_STRING:
 
-            if is_domain_description:
-                prompt = f'Solely based on the following text which relationships does this entity: "{entity_name}" have? '
-                prompt += f'Output only those relationships which you are certain about in JSON format like this: '
-            else:
+            if not is_domain_description:
                 prompt = f'Which relationships does the source entity: "{entity_name}" have? Output exactly {str(count_items_to_suggest)} relationships in JSON format like this: '
 
-            if True: #if is_provided_entity_source:
-                #prompt = '[{"name": first relationship name in form of a verb, "description": first relationship description, "source": "' + entity_name + '", "target": first relationship target entity}, {"name": second relationship name in form of a verb, "description": second relationship description, "source": "' + entity_name + '", "target": second relationship target entity}, ...].'
-                #prompt = '[{"name": first relationship name in form of a verb, "description": first relationship description, "source": "' + entity_name + '", "target": first relationship target entity represented as noun in singular}, {"name": second relationship name in form of a verb, "description": second relationship description, "source": "' + entity_name + '", "target": second relationship target entity represented as noun in singular}, ...].'
-                #prompt = '[{"name": first relationship name in form of a verb, "source": "' + entity_name + '", "target": first relationship target entity represented as noun in singular}, {"name": second relationship name in form of a verb, "source": "' + entity_name + '", "target": second relationship target entity represented as noun in singular}, ...].'
+            else:
+                prompt = f'Solely based on the following text which relationships does this entity: "{entity_name}" have? '
+                #prompt += f'Always make sure that the entity: "{entity_name}" is the source entity in all the relationships. '
+                #prompt += f'Output only those relationships which you are certain about in JSON format like this: '
+                prompt += f'Output it in JSON format like this: '
+            
+            names = ["name", "source", "target"]
+            if True: #not is_domain_description:
+                names.append("sentence")
 
-                prompt += JSONBuilder.build(
-                    names=["name", "source", "target", "sentence"],
-                    descriptions=["* relationship name in form of a verb", f'"{entity_name}"', f"* relationship target entity", "the short meaningful sentence for the * relationship"], times_to_repeat=times_to_repeat, is_elipsis=is_elipsis)
+            prompt += JSONBuilder.build(
+                names=names,
+                descriptions=["* relationship name in form of a verb", f'"{entity_name}"', f"* relationship target entity", "the short meaningful sentence for the * relationship"], times_to_repeat=times_to_repeat, is_elipsis=is_elipsis)
+                #descriptions=["relationship name in form of a verb", f'"{entity_name}"', f"relationship target entity", "the short meaningful sentence for the relationship"], times_to_repeat=times_to_repeat, is_elipsis=is_elipsis)
+    
         
+
         elif user_choice == RELATIONSHIPS_STRING_TWO_ENTITIES:
             if not is_domain_description:
-                prompt = 'What relationships could be between source entity "' + entity_name + '" and target entity "' + entity_name_2 + '"? '
+                prompt = 'What relationships are between source entity "' + entity_name + '" and target entity "' + entity_name_2 + '"? '
                 #prompt += 'Output exactly ' + str(count_items_to_suggest) + ' relationships in JSON format like this: [{"name": first relationship name where "' + source_entity + '" is the source entity and "' + target_entity + '" is the target entity, "sentence": short meaningful sentence where you put the first relationship name in between the source entity and the target entity in this order}, {"name": second relationship name where "' + source_entity + '" is the source entity and "' + target_entity + '" is the target entity, "sentence": short meaningful sentence where you put the first relationship name in between the source entity and the target entity in this order}, ...]. '
                 #prompt += 'Output exactly ' + str(count_items_to_suggest) + ' relationships in JSON format like this: [{"name": first relationship name where "' + source_entity + '" is the source entity and "' + target_entity + '" is the target entity, "sentence": the short meaningful sentence for the first relationship}, {"name": second relationship name where "' + source_entity + '" is the source entity and "' + target_entity + '" is the target entity, "sentence": the short meaningful sentence for the first relationship}, ...]. '
                 prompt += 'Output exactly ' + str(count_items_to_suggest) + ' relationships in JSON format like this: '
 
             else:
-                prompt = f'Solely based on the following text which relationships could be between the entity "{entity_name}" and the entity "{entity_name_2}"? '
+                prompt = f'Solely based on the following text which relationships are between the entity "{entity_name}" and the entity "{entity_name_2}"? '
                 prompt += f'Output only those relationships which you are certain about in JSON format like this: '
 
             prompt += JSONBuilder.build(names=["name", "source", "target", "sentence"], descriptions=[f'* relationship name where "{entity_name}" is the source entity and "{entity_name_2}" is the target entity', f'"{entity_name}"', f'"{entity_name_2}"', f'the short meaningful sentence for the first relationship where the source entity "{entity_name}" comes first then follows the relationship name and then follows the target entity "{entity_name_2}"'], times_to_repeat=times_to_repeat, is_elipsis=is_elipsis)
@@ -287,7 +309,7 @@ class UserInputProcessor():
         if user_message.lower() == "exit" or user_message.lower() == "quit" or user_message.lower() == "q":
             return False
         
-        self.user_choice = "a" #input("Input 'a' for attributes, 'r' for relationships, 'x' for relationships between two classes: ").lower()
+        self.user_choice = input("Input 'a' for attributes, 'r' for relationships, 'x' for relationships between two classes: ").lower()
 
         if self.user_choice == "a":
             self.user_choice = ATTRIBUTES_STRING
@@ -346,4 +368,32 @@ class JSONBuilder:
         if is_elipsis:
             result += ", ..."
         result += "]"
+        return result
+    
+
+    def build2(names, descriptions, times_to_repeat, is_elipsis=False):
+        # like `build` but as an example show first JSON object, elipsis, last JSON object
+        positions = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"]
+
+        result = "["
+        for i in range(times_to_repeat):
+            result += '{'
+            for j in range(len(names)):
+                current_description = descriptions[j].replace('*', positions[i])
+                result += '"' + names[j] + '": ' + current_description
+                if j + 1 < len(names):
+                    result += ', '
+            result += '}'
+            if i + 1 < times_to_repeat:
+                result += ', '
+        
+        if is_elipsis:
+            result += ", ... , {"
+            for j in range(len(names)):
+                    current_description = descriptions[j]
+                    result += '"' + names[j] + '": ' + current_description
+                    if j + 1 < len(names):
+                        result += ', '
+
+        result += "}]"
         return result
