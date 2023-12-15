2bda3eb0e5669cfcbecc250a7ff1c89f81e1cc20
Dominik7131 <dom.mod@seznam.cz>
2023-12-15 20:29:34 +0100

    File updated

--- client_tester_multiple.py
+++ client_tester_multiple.py
@@ -1,6 +1,5 @@
 from LLM_assistant import LLMAssistant, ITEMS_COUNT, ATTRIBUTES_STRING, RELATIONSHIPS_STRING, RELATIONSHIPS_STRING_TWO_ENTITIES, TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION
 import time
-import json
 
 IS_SAVE_TO_FILE = True
 
@@ -11,6 +10,10 @@ def main():
     model_file = "llama-2-7b-chat.Q5_K_M.gguf"
     model_type = "llama"
 
+    model_path_or_repo_id = "TheBloke/Starling-LM-7B-alpha-GGUF"
+    model_file = "starling-lm-7b-alpha.Q5_K_M.gguf"
+    model_type ="openchat"
+
     llm_assistant = LLMAssistant(model_path_or_repo_id=model_path_or_repo_id, model_file=model_file, model_type=model_type)
 
     entities = {
@@ -20,25 +23,28 @@ def main():
         }
 
 
-    domain_descriptions = ["", "We know that courses have a name and a specific number of credits. Each course can have one or more professors, who have a name. Professors could participate in any number of courses. For a course to exist, it must aggregate, at least, five students, where each student has a name. Students can be enrolled in any number of courses. Finally, students can be accommodated in dormitories, where each dormitory can have from one to four students. Besides, each dormitory has a price."]
+    domain_descriptions = ["We know that courses have a name and a specific number of credits. Each course can have one or more professors, who have a name. Professors could participate in any number of courses. For a course to exist, it must aggregate, at least, five students, where each student has a name. Students can be enrolled in any number of courses. Finally, students can be accommodated in dormitories, where each dormitory can have from one to four students. Besides, each dormitory has a price."]
     user_choices = [ATTRIBUTES_STRING, RELATIONSHIPS_STRING, RELATIONSHIPS_STRING_TWO_ENTITIES]
 
     file_name = f"output/{time.strftime('%Y-%m-%d-%H-%M-%S')}.txt"
 
     if (IS_SAVE_TO_FILE):
         with open(file_name, 'w') as file:
-            file.write(f"Take only relevant info from domain description: {str(TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION)}\n\n")
+            file.write(f"LLM ID: {model_path_or_repo_id}\nLLM file: {model_file}\nTake only relevant info from domain description: {str(TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION)}\n\n")
+
+    time_start = time.time()
 
     for user_choice in user_choices:
         for domain_description in domain_descriptions:
             for entity in entities[user_choice]:
 
+                entity_1 = entity
                 entity_2 = ""
                 if user_choice == RELATIONSHIPS_STRING_TWO_ENTITIES:
+                    entity_1 = entity[0]
                     entity_2 = entity[1]
-                    entity = entity[0]
 
-                items = llm_assistant.suggest(entity, entity_2, user_choice, ITEMS_COUNT, conceptual_model=[], domain_description=domain_description)
+                items = llm_assistant.suggest(entity_1, entity_2, user_choice, ITEMS_COUNT, conceptual_model=[], domain_description=domain_description)
                 
                 if (IS_SAVE_TO_FILE):
                     with open(file_name, 'a') as file:
@@ -52,6 +58,8 @@ def main():
                         
                         file.write("\n\n")
 
+    time_end = time.time()
+    print(f"\nTime: {time_end - time_start:.2f} seconds")
 
 if __name__ == "__main__":
     main()
--- client_tester_single.py
+++ client_tester_single.py
@@ -1,8 +1,6 @@
 from LLM_assistant import LLMAssistant, ITEMS_COUNT, ATTRIBUTES_STRING, RELATIONSHIPS_STRING, RELATIONSHIPS_STRING_TWO_ENTITIES, TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION
 import time
-import json
 
-IS_SAVE_TO_FILE = True
 
 def main():
 
@@ -11,33 +9,30 @@ def main():
     model_file = "llama-2-7b-chat.Q5_K_M.gguf"
     model_type = "llama"
 
+    model_path_or_repo_id = "TheBloke/Starling-LM-7B-alpha-GGUF"
+    model_file = "starling-lm-7b-alpha.Q5_K_M.gguf"
+    model_type ="openchat"
+
     llm_assistant = LLMAssistant(model_path_or_repo_id=model_path_or_repo_id, model_file=model_file, model_type=model_type)
 
-    entities = ["vehicle", "student", "gpu", "employee", "earth", "zoo", "amusement park", "thing"]
-    user_choices = [ATTRIBUTES_STRING, RELATIONSHIPS_STRING, RELATIONSHIPS_STRING_TWO_ENTITIES]
-
-    file_name = f"output/{time.strftime('%Y-%m-%d-%H-%M-%S')}.txt"
-    if (IS_SAVE_TO_FILE):
-        with open(file_name, 'w') as file:
-            file.write(f"Take only relevant info from domain description: {str(TAKE_ONLY_RELEVANT_INFO_FROM_DOMAIN_DESCRIPTION)}\n\n")
-
+    #entities = ["vehicle", "student", "gpu", "employee", "earth", "zoo", "amusement park", "thing"]
+    #entities = [("professor", "student")]
+    entities = ["course", "professor", "dormitory", "student"]
+    user_choice = ATTRIBUTES_STRING
 
     for entity in entities:
         time_start = time.time()
 
+        entity_1 = entity
+        entity_2 = ""
+        if user_choice == RELATIONSHIPS_STRING_TWO_ENTITIES:
+            entity_1 = entity[0]
+            entity_2 = entity[1]
         domain_description = "We know that courses have a name and a specific number of credits. Each course can have one or more professors, who have a name. Professors could participate in any number of courses. For a course to exist, it must aggregate, at least, five students, where each student has a name. Students can be enrolled in any number of courses. Finally, students can be accommodated in dormitories, where each dormitory can have from one to four students. Besides, each dormitory has a price."
-        items = llm_assistant.suggest(entity, "", ATTRIBUTES_STRING, ITEMS_COUNT, conceptual_model=[], domain_description=domain_description)
+        llm_assistant.suggest(entity_1, entity_2, user_choice, ITEMS_COUNT, conceptual_model=[], domain_description=domain_description)
 
         time_end = time.time()
         print(f"\nTime: {time_end - time_start:.2f} seconds")
-        
-        if (IS_SAVE_TO_FILE):
-            with open(file_name, 'a') as file:
-                for item in items:
-                    #json.dump(item, file)
-                    file.write(str(item) + '\n')
-                
-                file.write("\n\n")
 
 
 if __name__ == "__main__":
