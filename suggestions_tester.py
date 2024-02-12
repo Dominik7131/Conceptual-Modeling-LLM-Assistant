8dd12ae40c4a9721bb2f4e8e03de91031fb740e0
Dominik7131 <dom.mod@seznam.cz>
2024-02-12 09:35:58 +0000

    File updated

--- suggestions_tester.py
+++ suggestions_tester.py
@@ -6,13 +6,13 @@ import json
 PATH_TO_DATA_DIRECTORY = "data/56-2001-extract-llm-assistant-test-case"
 INPUT_DOMAIN_DESCRIPTION_FILE_PATH = f"{PATH_TO_DATA_DIRECTORY}/56-2001-extract-llm-assistant-test-case.txt"
 IS_A_RELATIONSHIPS_STRING = "is_a_relationships"
-ACTUAL_OUTPUT = "actual_output"
-EXPECTED_OUTPUT = "expected_output"
+ACTUAL_OUTPUT = "actual-output"
+EXPECTED_OUTPUT = "expected-output"
 TIMESTAMP_PREFIX = time.strftime('%Y-%m-%d-%H-%M-%S')
 
 # Settings
 IS_GENERATE_EXPECTED_OUTPUT = False
-USER_CHOICE = ATTRIBUTES_STRING
+USER_CHOICE = IS_A_RELATIONSHIPS_STRING
 
 
 def generate_expected_output(test_file_path, output_file_path):
@@ -63,7 +63,7 @@ def main():
     test_data_attributes = test_data[USER_CHOICE]
 
     with open(actual_output_file_path, 'w') as file:
-        for test_case in test_data_attributes:
+        for test_case in test_data_attributes[2:]:
             entity_name = test_case['entity']
             file.write(f"Entity: {entity_name}\n")
 
