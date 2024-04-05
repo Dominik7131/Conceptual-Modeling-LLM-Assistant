6ad63585cfeb7edcd459cfcb2cb08c6034561655
Dominik7131 <dom.mod@seznam.cz>
2024-04-05 10:39:10 +0200

    Fix bugs with original text indexes merging

--- text_utility.py
+++ text_utility.py
@@ -531,43 +531,88 @@ class TextUtility:
             return result
 
     
+    def are_tuples_sorted(tuples):
+
+        last_x1 = 0
+        last_x2 = 0
+
+        for i in range(len(tuples)):
+            x1 = tuples[i][0]
+            x2 = tuples[i][1]
+
+            if x1 < last_x1:
+                return False
+
+            if x1 == last_x1 and x2 < last_x2:
+                return False
+            
+            last_x1 = x1
+            last_x2 = x2
+    
+        return True
+
+
+
+
     def merge_original_texts(input):
 
-        sorted_input = sorted(input, key=lambda x: (x[0], x[1]))
+        input = sorted(input, key=lambda x: (x[0], x[1]))
 
         i = 0
-        while i < len(sorted_input):
+        while i < len(input):
 
-            if i + 1 >= len(sorted_input):
+            if i + 1 >= len(input):
                 break
 
             # [(x1, x2, l1), (y1, y2, l2)]
-            x1 = sorted_input[i][0]
-            x2 = sorted_input[i][1]
-            l1 = sorted_input[i][2]
-            y1 = sorted_input[i + 1][0]
-            y2 = sorted_input[i + 1][1]
-            l2 = sorted_input[i + 1][2]
+            x1 = input[i][0]
+            x2 = input[i][1]
+            l1 = input[i][2]
+            y1 = input[i + 1][0]
+            y2 = input[i + 1][1]
+            l2 = input[i + 1][2]
 
-            if x2 == y1:
+            is_still_sorted = x1 < y1 or (x1 == y1 and x2 <= y2)
+
+            if not is_still_sorted:
+                # E.g.: Input: [(2, 4, "l1"), (2, 6, "l2"), (2, 8, "l3")]
+                # After first step: [(2, 4, "l1, l2"), (5, 6, "l2"), (2, 8, "l3")]
+                # - first index in second tuple is greater than first index in third tuple
+                # -> we need to sort the tuples again and start from the beginning
+                input = sorted(input, key=lambda x: (x[0], x[1]))
+                i = 0
+
+            elif x1 == y1:
+
+                if x2 == y2:
+                    merged_tuples = (x1, x2, f"{l1}, {l2}")
+                    input[i] = merged_tuples
+                    input.pop(i + 1)
+
+                else:
+                    merged_tuples = (x1, x2, f"{l1}, {l2}")
+                    input[i] = merged_tuples
+                    input[i + 1] = (x2 + 1, y2, l2)
+
+            elif x2 == y1:
                 merged_tuples = (x1, y2, f"{l1}, {l2}")
-                sorted_input[i] = merged_tuples
-                sorted_input.pop(i + 1)
+                input[i] = merged_tuples
+                input.pop(i + 1)
             
             elif x2 > y1 and x2 >= y2:
                 merged_tuples = (x1, x2, f"{l1}, {l2}")
-                sorted_input[i] = merged_tuples
-                sorted_input.pop(i + 1)
+                input[i] = merged_tuples
+                input.pop(i + 1)
             
             elif x2 > y1 and x2 < y2:
                 merged_tuples = (y1, x2, f"{l1}, {l2}")
-                sorted_input[i] = (x1, y1 - 1, l1)
-                sorted_input.insert(i + 1, merged_tuples)
-                sorted_input[i + 2] = (x2 + 1, y2, l2)
+                input[i] = (x1, y1 - 1, l1)
+                input.insert(i + 1, merged_tuples)
+                input[i + 2] = (x2 + 1, y2, l2)
 
             else:
                 i += 1
 
-        return sorted_input
+        return input
 
         
