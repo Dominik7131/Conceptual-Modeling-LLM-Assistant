7da78ed4748d8368d8e122c08c819e511e9806bb
Dominik7131 <dom.mod@seznam.cz>
2024-04-25 16:25:37 +0200

    Refactor semantic text filtering

--- semantic_text_filterer.py
+++ semantic_text_filterer.py
@@ -1,110 +1,57 @@
 from sentence_transformers import SentenceTransformer, util
-from termcolor import cprint
 from text_utility import TextUtility
-from RAG_tester import RAGTester
-import logging
 import os
-import numpy as np
 
 
-logging.basicConfig(level=logging.INFO, format="%(message)s")
-
-SCORE_NECESSARY_THRESHOLD = 0.2
-SCORE_MIN_THRESHOLD = 0.5
-RANGE_FROM_TOP = 0.4 # E.g. if max score is 0.7 then invalidate any text with score lower than 0.7 - `RANGE_FROM_TOP`
-IS_CACHING = False
-PATH_TO_DATA_DIRECTORY = os.path.join("data", "56-2001-extract-llm-assistant-test-case")
-INPUT_FILE_PATH = os.path.join(PATH_TO_DATA_DIRECTORY, "56-2001-extract-llm-assistant-test-case.txt")
-OUTPUT_FILE_NAME = "out.txt"
-IS_TESTING = False
-
-ENTITY = "structural component"
-
-def print_green_on_black(x):
-    return cprint(x, 'green', 'on_black')
-
-def print_yellow_on_black(x):
-    return cprint(x, 'yellow', 'on_black')
-
-def print_red_on_black(x):
-    return cprint(x, 'red', 'on_black')
+SCORE_NECESSARY_THRESHOLD = 0.1
+RANGE_FROM_TOP = 0.55 # E.g. if max score is 0.7 then invalidate any text with score lower than 0.7 - `RANGE_FROM_TOP`
 
 
-def show_result(scores, chunks, min_score_threshold):
-    is_any_score_higher_than_threshold = False
+class SemanticTextFilterer:
 
-    for i in range(len(chunks)):
-        if scores[0][i] > min_score_threshold:
-            is_any_score_higher_than_threshold = True
-            break
+    def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        # self.model = SentenceTransformer('all-mpnet-base-v2') # Symmetric
+        # self.model = SentenceTransformer('msmarco-distilbert-base-v4') # Asymmetric
+    
+    def encode(self, queries):
+        self.model.encode(queries, convert_to_tensor=True)
+    
 
-    if not is_any_score_higher_than_threshold:
-        return show_result(scores, chunks, min_score_threshold - 0.05)
+    def get(self, entity, domain_description):
 
-    with open(OUTPUT_FILE_NAME, "w") as output:
-        max_score = scores[0].max()
+        chunks = TextUtility.split_into_sentences(domain_description)
+        query = f"Info about {entity}"
 
-        previous_written_chunk_index = 0
+        queries_embeddings = self.model.encode(query, convert_to_tensor=True)
+        chunks_embeddings = self.model.encode(chunks, convert_to_tensor=True)
+
+        scores = util.cos_sim(queries_embeddings, chunks_embeddings)
+        max_score = scores[0].max().item()
+
+        threshold = max(max_score - RANGE_FROM_TOP, SCORE_NECESSARY_THRESHOLD)
+
+        result = []
+
         for i in range(len(chunks)):
-            score = scores[0][i]
-            msg = f"Score: {score} | {chunks[i]}"
-
-
-            if score > SCORE_NECESSARY_THRESHOLD and score > max_score - RANGE_FROM_TOP:
-                # Separate consecutive chunks by whitespace
-                if previous_written_chunk_index + 1 == i:
-                    output.write(f"{chunks[i]} ")
-
-                # Separate non-consecutive chunks by a new line
-                else:
-                    output.write(f"{chunks[i]}\n")
-                
-                previous_written_chunk_index = i
-
-
-            if score >= min_score_threshold:
-                print_green_on_black(msg)
-            elif score <= SCORE_NECESSARY_THRESHOLD:
-                print_red_on_black(msg)
-            else:
-                print(msg)
+            score = scores[0, i]
+            
+            if score > threshold:
+                result.append(chunks[i])
+        
+        return result
 
 
 def main():
 
-    model = SentenceTransformer('all-MiniLM-L6-v2') # Symmetric
-    # model = SentenceTransformer('all-mpnet-base-v2') # Symmetric
-    # model = SentenceTransformer('msmarco-distilbert-base-v4') # Asymmetric
-    
-    chunks = TextUtility.split_file_into_chunks(INPUT_FILE_PATH)
+    # Simple example of usage
+    path = os.path.join("domain-modeling-benchmark", "domain-models", "farming 97627e23829afb", "domain-description-01.txt")
+    with open(path) as file:
+        domain_description = file.read()
 
-    if IS_TESTING:
-        RAGTester.test_RAG_approach(model, chunks, SCORE_NECESSARY_THRESHOLD, RANGE_FROM_TOP)
-        return
-
-    query = TextUtility.create_query(ENTITY)
-    queries = [query]
-    queries_embeddings = model.encode(queries, convert_to_tensor=True)
-
-    chunks_embeddings = None
-    if IS_CACHING:
-        cached_file_name = f"{INPUT_FILE_PATH}_embeddings.npy"
-        if os.path.isfile(cached_file_name):
-            logging.debug(f"Using cached embeddings from: {cached_file_name}")
-            chunks_embeddings = np.load(cached_file_name)
-        else:
-            logging.debug(f"Saving embeddings into cache")
-            chunks_embeddings = model.encode(chunks, convert_to_tensor=True)
-            np.save(cached_file_name, chunks_embeddings)
-    else:
-        chunks_embeddings = model.encode(chunks, convert_to_tensor=True)
-
-
-
-    scores = util.cos_sim(queries_embeddings, chunks_embeddings)
-
-    show_result(scores, chunks, SCORE_MIN_THRESHOLD)
-    print()
+    filterer = SemanticTextFilterer()
+    entity = "farmer"
+    actual_texts = filterer.get(entity, domain_description)
 
 
 if __name__ == "__main__":
--- tests/relevant_text_finder_tester.py
+++ tests/relevant_text_finder_tester.py
@@ -4,6 +4,7 @@ import json
 import os
 from text_utility import DomainDescriptionFilteringVariation, TextUtility
 from syntactic_text_filterer import SyntacitTextFilterer
+from semantic_text_filterer import SemanticTextFilterer
 
 PATH_TO_DATA_DIRECTORY = os.path.join("data", "56-2001-extract-llm-assistant-test-case")
 TEST_DATA_FILE_PATH = os.path.join(PATH_TO_DATA_DIRECTORY, "relevant_texts.json")
@@ -22,6 +23,9 @@ class RAGTester:
 
         if filtering_variation == DomainDescriptionFilteringVariation.SYNTACTIC:
             syntactic_text_finder = SyntacitTextFilterer()
+        
+        if filtering_variation == DomainDescriptionFilteringVariation.SEMANTIC:
+            semantic_text_finder = SemanticTextFilterer()
 
         total_tests = 0
         successful_tests = 0
@@ -62,7 +66,7 @@ class RAGTester:
                         actual_relevant_texts = syntactic_text_finder.get(entity, domain_description)
 
                     elif filtering_variation == DomainDescriptionFilteringVariation.SEMANTIC:
-                        raise ValueError("Not implemented")
+                        actual_relevant_texts = semantic_text_finder.get(entity, domain_description)
 
                     else:
                         raise ValueError(f"Unexpected filtering variation: {filtering_variation}")
@@ -112,78 +116,6 @@ class RAGTester:
                 print(text)
             print()
             print()
-
-
-    def test_semantic_filtering(model, chunks, SCORE_NECESSARY_THRESHOLD, RANGE_FROM_TOP):
-
-        # Import the module here so it is not loaded when not using this method
-        from sentence_transformers import util
-
-        test_cases = RAGTester.load_test_cases()
-        are_all_tests_passing = True
-
-        # "test_cases": [{"entity1": "", "relevant_texts": []}, {"entity2": "", "relevant_texts": [] }]
-        # relevant_texts' -- list of relevant texts which must be in the RAG output
-        
-        queries = []
-        for test_case in test_cases:
-            entity = test_case['entity']
-            queries.append(TextUtility.create_query(entity))
-
-        queries_embeddings = model.encode(queries, convert_to_tensor=True)
-        chunks_embeddings = model.encode(chunks, convert_to_tensor=True)
-
-        scores = util.cos_sim(queries_embeddings, chunks_embeddings)
-        min_score_in_relevant_texts_query_index = 0
-        min_score_in_relevant_texts_chunk_index = 0
-        min_score = 1
-        min_score_text = ""
-
-        max_range_distance = 0
-        max_score_text = ""
-
-        for test_case_index, test_case in enumerate(test_cases):
-            relevant_texts = test_case['relevant_texts']
-
-            max_score = scores[test_case_index].max().item()
-
-            for text in relevant_texts:
-
-                # Get chunk ID for the given text
-                chunk_index = -1
-                for i, chunk in enumerate(chunks):
-                    if text in chunk:
-                        chunk_index = i
-                        # print(f"Found:\n- relevant text: {text}\n- chunk: {chunk}\n")
-                        break
-                
-                if chunk_index == -1:
-                    print(f"Error: relevant text not found in any of the chunks: \"{text}\"\nchunks:\n{chunks}")
-                    continue
-                
-                score = scores[test_case_index, chunk_index]
-
-                if score < min_score:
-                    min_score_in_relevant_texts_query_index = test_case_index
-                    min_score_in_relevant_texts_chunk_index = chunk_index
-                    min_score_text = text
-                    min_score = score
-                
-                threshold = max_score - RANGE_FROM_TOP
-                if abs(score - threshold) > max_range_distance:
-                    max_range_distance = abs(score - threshold)
-                    max_score_text = text
-
-                if score <= SCORE_NECESSARY_THRESHOLD or score < max_score - RANGE_FROM_TOP:
-                    print(f"Test failed:\n- query: {queries[test_case_index]}\n- relevant text: {text}\n- score: {score}")
-                    print(f"- max score: {max_score}")
-                    are_all_tests_passing = False
-        
-        if are_all_tests_passing:
-            print("All tests are passing")
-        
-        print(f"Minimal score in relevant texts: {scores[min_score_in_relevant_texts_query_index, min_score_in_relevant_texts_chunk_index]}\n- query: {queries[min_score_in_relevant_texts_query_index]}\n- text: {min_score_text}\n")
-        print(f"Max range distance: {max_range_distance}\n- text: {max_score_text}")
     
 
     def load_test_cases():
@@ -196,7 +128,7 @@ class RAGTester:
 
 def main():
 
-    filtering_variation = DomainDescriptionFilteringVariation.SYNTACTIC
+    filtering_variation = DomainDescriptionFilteringVariation.SEMANTIC
     print(f"Selected filtering variation: {filtering_variation}")
 
     RAGTester.test_filtering(filtering_variation)
