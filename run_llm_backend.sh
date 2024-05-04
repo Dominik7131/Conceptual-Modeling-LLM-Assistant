a4530e664c906bde33d83e1856b22f89cd03e04f
Dominik7131 <dom.mod@seznam.cz>
2024-05-04 11:47:51 +0200

    Update file

--- run_llm_backend.sh
+++ run_llm_backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./server --batch-size 8192
 set -euo pipefail
+
 
 cd llama.cpp/
 
@@ -9,6 +9,8 @@ cd llama.cpp/
 # MODEL_PATH=/data/LLMs/models--TheBloke--Llama-2-7B-Chat-GGUF/llama-2-7b-chat.Q4_K_M.gguf
 
 MODEL_PATH=/data/LLMs/models--TheBloke--Mixtral-8x7B-Instruct-v0.1-GGUF/mixtral-8x7b-instruct-v0.1.Q5_K_M.gguf
+# MODEL_PATH=/data/LLMs/Llama-3-8B-GGUF/Meta-Llama-3-8B-Instruct.Q8_0.gguf
+
 BATCH_SIZE=8192
 UBATCH_SIZE=4096
 N_GPU_LAYERS=100
