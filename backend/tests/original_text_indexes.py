1906defb1ca3f29779e8e2ac69fc8e8788ca9d7e
Dominik7131 <dom.mod@seznam.cz>
2024-05-25 21:15:14 +0200

    Update script

--- backend/tests/original_text_indexes.py
+++ backend/tests/original_text_indexes.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append('.')
-from text_utility import TextUtility
+sys.path.append('utils/')
+sys.path.append('backend/utils/')
+from text_utility import TextUtility, UserChoice
 import os
 import logging
 
@@ -11,7 +13,7 @@ import logging
 # Each test has: inference text, [index: start of this inference in domain description, index: end of this inference in domain description]
 
 def test(inference, domain_description, expected_inference_indexes):
-    actual_inference_indexes, _, _ = TextUtility.find_text_in_domain_description(inference, domain_description)
+    actual_inference_indexes, _, _ = TextUtility.find_text_in_domain_description(inference, domain_description, user_choice=UserChoice.ATTRIBUTES.value)
     if not actual_inference_indexes == expected_inference_indexes:
         print(f"Test failed:\n- domain description: {domain_description}\n- inference: {inference}\n- expected inference indexes: {expected_inference_indexes}\n- actual inference indexes: {actual_inference_indexes}\n")
         TextUtility.show_inference_in_domain_description(actual_inference_indexes, domain_description)
@@ -71,17 +73,9 @@ def basic_tests():
     test(inference, domain_description, expected_inference_indexes)
 
     # Temp
-    data_directory_path = os.path.join("data", "56-2001-extract-llm-assistant-test-case")
-    file_name = "56-2001-extract-llm-assistant-test-case.txt"
-    domain_description_file = os.path.join(data_directory_path, file_name)
-
-    with open(domain_description_file, 'r') as file:
-        domain_description = file.read().lower()
-
-    # domain_description = "In addition, for a road vehicle it is stated:\n- the registration plate, the date on which the registration plate was assigned,\n- the purpose for which the road vehicle is intended, which is a taxi service vehicle, a vehicle with right of way, a car rental vehicle for hire, a vehicle for general use, or a vehicle for the operation of road transport for hire by its operator,\n- the category of road vehicle,\n- the manufacturer's factory mark as specified by the road vehicle manufacturer,\n- the trade name if specified by the road vehicle manufacturer,\n- the type as specified by the road vehicle manufacturer and the variant and version if specified by the road vehicle manufacturer.\n- the vehicle identification number (VIN) of the road vehicle and, if not, the chassis serial number of the road vehicle,\n- the engine data for the road vehicle that include the type of engine specified by the manufacturer of the road vehicle, the engine power specified by the road vehicle manufacturer in kW/rpm in the case of an internal combustion engine or in kW in the case of other engines, the displacement of the internal combustion engine as specified by the manufacturer of the road vehicle in cm3; and the fuel type of the road vehicle\n- type, serial number and colour of bodywork, number of seats and standing places or beds, dimensions of the loading area, volume of the box or tank,\n- data on technical inspections of the road vehicle which is the type of technical inspection carried out, the date on which the technical inspection was carried out, the date of validity of the technical inspection, the designation of the technical inspection station and data on the condition of the road vehicle according to the result of the technical inspection,"
-        
-    inference = "it is not listed as missing or stolen in the Schengen Information System or in the information system of the Police of the Czech Republic or this information is not established by another procedure"
-    expected_inference_indexes = []
+    domain_description = "A farmer is an individual engaged in agriculture, growing and harvesting crops, and is identified uniquely by a name that is used to refer"
+    inference = "A farmer is identified uniquely by a name"
+    expected_inference_indexes = [0, 8, 84, 116]
     test(inference, domain_description, expected_inference_indexes)
 
 
