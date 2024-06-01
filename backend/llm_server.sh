37cf1d5b783dfb3ba8f3c3f326b6372c9cc9e382
Dominik7131 <dom.mod@seznam.cz>
2024-06-01 20:21:51 +0200

    Rename file

--- backend/llm_server.sh
+++ backend/llm_server.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 cd llama.cpp/
 
 MODEL_PATH=/data/LLMs/models--TheBloke--Mixtral-8x7B-Instruct-v0.1-GGUF/mixtral-8x7b-instruct-v0.1.Q5_K_M.gguf
+PORT=8080
 BATCH_SIZE=8192
 UBATCH_SIZE=4096
 N_GPU_LAYERS=44
@@ -15,4 +16,4 @@ SYSTEM_PROMPT_FILE=../../prompts/system_prompt_default.json
 LOG_FORMAT=text
 TOKENS_TO_PREDICT=8192
 
-./server --model $MODEL_PATH --batch-size $BATCH_SIZE --ubatch-size $UBATCH_SIZE --n-gpu-layers $N_GPU_LAYERS --ctx-size $CONTEXT_SIZE --chat-template $CHAT_TEMPLATE --system-prompt-file $SYSTEM_PROMPT_FILE --log-format $LOG_FORMAT --n-predict $TOKENS_TO_PREDICT
+./server --model $MODEL_PATH --port $PORT --batch-size $BATCH_SIZE --ubatch-size $UBATCH_SIZE --n-gpu-layers $N_GPU_LAYERS --ctx-size $CONTEXT_SIZE --chat-template $CHAT_TEMPLATE --system-prompt-file $SYSTEM_PROMPT_FILE --log-format $LOG_FORMAT --n-predict $TOKENS_TO_PREDICT
