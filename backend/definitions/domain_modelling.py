4f65e9f9d0c62d23dbd595cc931c45bfd4abfd56
Dominik7131 <dom.mod@seznam.cz>
2024-06-04 11:17:52 +0200

    Update directory name

--- backend/definitions/domain_modelling.py
+++ backend/definitions/domain_modelling.py
@@ -2,7 +2,7 @@ import os
 
 DOMAIN_MODELING_BENCHMARK_DIRECTORY = "domain-modeling-benchmark"
 DOMAIN_MODELING_DIRECTORY_PATH = os.path.join(
-    DOMAIN_MODELING_BENCHMARK_DIRECTORY, "evaluation domain models")
+    DOMAIN_MODELING_BENCHMARK_DIRECTORY, "manual evaluation domains")
 
 DOMAIN_MODELS = ["aircraft manufacturing 48982a787d8d25", "conference papers 56cd5f7cf40f52", "farming 97627e23829afb",
                  "college 1dc8e791-1d0e-477c-b5c2-24e376e3f6f1", "zoological gardens e95b5ea472deb8",
@@ -16,5 +16,5 @@ DOMAIN_DESCRIPTIONS_COUNT = [3, 3, 3, 1, 1, 1]
 DOMAIN_TEXTS_COUNT = 12
 
 DOMAIN_PROMPTING_DIRECTORY_PATH = os.path.join(
-    DOMAIN_MODELING_BENCHMARK_DIRECTORY, "prompting domain models")
+    DOMAIN_MODELING_BENCHMARK_DIRECTORY, "prompting domains")
 PROMPTING_MODEL_NAME = "company employees 4ffd4466-50ec-4d98-b2c1-c3fdba90a65c"
