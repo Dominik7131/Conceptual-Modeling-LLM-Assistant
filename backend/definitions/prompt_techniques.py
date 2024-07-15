458c6ebbd673d1b1aa6d245f58c6701b2934cc7d
Dominik7131 <dom.mod@seznam.cz>
2024-07-15 11:35:05 +0200

    Add script for selecting prompt techniques

---
+++ backend/definitions/prompt_techniques.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from definitions.utility import UserChoice
+
+
+class PromptTechniques(Enum):
+    BASELINE = "baseline"
+    COT = "cot"
+    N_SHOT = "n-shot"
+    N_SHOT_COT = "n-shot-cot"
+
+
+SELECTED_PROMPT_TECHNIQUES = {
+    UserChoice.CLASSES.value: PromptTechniques.N_SHOT.value,
+    UserChoice.ATTRIBUTES.value: PromptTechniques.N_SHOT_COT.value,
+    UserChoice.ASSOCIATIONS_ONE_KNOWN_CLASS.value: PromptTechniques.N_SHOT_COT.value,
+    UserChoice.ASSOCIATIONS_TWO_KNOWN_CLASSES.value: PromptTechniques.N_SHOT_COT.value,
+}
