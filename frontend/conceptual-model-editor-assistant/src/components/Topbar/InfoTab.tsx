668eb839b1763812d99240a6953a9a5f2d5f4c2c
Dominik7131 <dom.mod@seznam.cz>
2024-07-15 11:43:06 +0200

    Update info

--- frontend/conceptual-model-editor-assistant/src/components/Topbar/InfoTab.tsx
+++ frontend/conceptual-model-editor-assistant/src/components/Topbar/InfoTab.tsx
@@ -6,7 +6,7 @@ const InfoTab: React.FC = (): JSX.Element =>
     return (
         <>
             <Typography>
-                If you have any issues please let us know on our <a href="https://github.com/Dominik7131/Conceptual-Modeling-LLM-Assistant/issues" target="_blank" rel="noopener noreferrer">GitHub page</a>.
+                If you find any issues please let us know on our <a href="https://github.com/Dominik7131/Conceptual-Modeling-LLM-Assistant/issues" target="_blank" rel="noopener noreferrer">GitHub page</a>.
             </Typography>
         </>
     )
